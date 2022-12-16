c17779a4446c2f58780719d069d21673e6eae59b
James Autor <119373982+kamoteng-kahoy@users.noreply.github.com>
2022-12-16 09:35:04 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
-$('#about').click(function(){
-    $(document).scrollTop(200)
-    });
+$('#home').click(function()
+{
+    $(document).scrollTop(0)
+});
+
+$('#about').click(function()
+{
+    $(document).scrollTop(250)
+});
+
+$('#exp').click(function()
+{
+    $(document).scrollTop(300)
+});
+
+$('#contact').click(function()
+{
+    $(document).scrollTop(350)
+});
